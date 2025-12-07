c768a698fc3e2821b58f8cff6e1ee70903865ed2
ianto-cannon <ianto.cannon@gmail.com>
2025-12-07 22:15:40 +0100

    loop over number of lobes

--- spinning_drop.py
+++ spinning_drop.py
@@ -1,7 +1,7 @@
 #Ianto Cannon 2025 Dec 1. Plot the profile of a levitating spinning drop, photographed in Fauzia Wardani's gallery of fluid motion at APS DFD.
 import numpy as np
 
-def spinning_drop_profile_solver(capillary_len, rad_tip, z_tip, nodes, fname=None):
+def spinning_drop_profile_solver(capillary_len, rad_tip, z_tip, lobes, fname=None):
   ds = 1e-4*z_tip
   psi=0
   r=0
@@ -22,15 +22,15 @@ def spinning_drop_profile_solver(capillary_len, rad_tip, z_tip, nodes, fname=Non
       if z < -z_tip: break
     else:
       if (psi-np.pi/2)*(psi-dPsi-np.pi/2) < 0:
-        nodes-=1
-        if nodes==0: break
+        lobes-=1
+        if lobes==0: break
     if psi < 0: break
     if psi > np.pi: break
     Volume += np.pi*r**2*dz
     centroid += z*np.pi*r**2*dz
   if fname: print('saved',fname,'iterations',i)
   centroid /= Volume
-  return Volume, r, z, centroid, psi, dPsi
+  return Volume, r, z, centroid, psi
 
 def plot_drop_profile(folder, fname): 
   import matplotlib.pyplot as plt
@@ -55,32 +55,27 @@ def plot_drop_profile(folder, fname):
 surf_tens = 23.2e-3 #N/m, surface tension
 density = 801 #kg*m**-3, density difference between the drop and the surrounding air
 z_tip = 1.18e-2 #m, distance of the tip from the axis of rotation
-nodes = 1 # 1,2,3,4..., dimensionless, number of times the profile is parallel to the z axis for z>0
 
 brown_txt = open('data/brown.txt', 'a') 
-for r in range(20):
-  rotation_speed = .10 + r #rad/s, rotation speed of the drop
-  #rotation_speed = 20 + 2*r #rad/s, rotation speed of the drop
-  #rotation_speed = 11.5 + .05*r #rad/s, rotation speed of the drop
-  #Use bisection to find rad_tip that centres the drop at z=0
-  capillary_len = ( surf_tens / density / rotation_speed**2 ) ** (1/3)
-  rad_min = z_tip/10
-  rad_max = z_tip*2
-  for t in range(20):
-    rad_tip = (rad_max + rad_min)/2
-    Volume, rad_neck, z, centroid, psi, dPsi = spinning_drop_profile_solver(capillary_len, rad_tip, z_tip, nodes)
-    if psi < 0: rad_max = rad_tip
-    elif psi > np.pi: rad_max = rad_tip
-    elif z > 0: rad_min = rad_tip
-    else: rad_max = rad_tip
-  #if (psi-np.pi/2)*(psi-dPsi-np.pi/2) > 0: break
-  #if z**2 > capillary_len**2: break
-  if z**2 > (.01*z_tip)**2: break
-  #if z > 0: break
-  #if psi < 0: break 
-  #if psi > np.pi: break
-  Volume, rad_neck, z, centroid, psi, dPsi = spinning_drop_profile_solver(capillary_len, rad_min, z_tip, nodes, fname=f'data/spin{r:03}.txt')
-  size = (3*Volume/4/np.pi)**(1/3) #m radius of drop if it were spherical
-  print( rotation_speed*(density*size**3/8/surf_tens)**.5, z_tip/size, file=brown_txt)
-  print( r, rotation_speed*(density*size**3/8/surf_tens)**.5, z_tip/size)
-plot_drop_profile('data/','spin')
+for lobes in range(1,5): # 1,2,3,4..., dimensionless, number of bulges in z
+  for r in range(10):
+    rotation_speed = .5 + .5*r #rad/s, rotation speed of the drop
+    #rotation_speed = 20 + 2*r #rad/s, rotation speed of the drop
+    #rotation_speed = 11.5 + .05*r #rad/s, rotation speed of the drop
+    #Use bisection to find rad_tip that centres the drop at z=0
+    capillary_len = ( surf_tens / density / rotation_speed**2 ) ** (1/3)
+    rad_min = z_tip/10
+    rad_max = z_tip*2
+    for t in range(20):
+      rad_tip = (rad_max + rad_min)/2
+      Volume, rad_neck, z, centroid, psi = spinning_drop_profile_solver(capillary_len, rad_tip, z_tip, lobes)
+      if psi < 0: rad_max = rad_tip
+      elif psi > np.pi: rad_max = rad_tip
+      elif z > 0: rad_min = rad_tip
+      else: rad_max = rad_tip
+    if z**2 > (.01*z_tip)**2: continue
+    Volume, rad_neck, z, centroid, psi = spinning_drop_profile_solver(capillary_len, rad_min, z_tip, lobes, fname=f'data/spinNodes{lobes:01}R{r:02}.txt')
+    size = (3*Volume/4/np.pi)**(1/3) #m radius of drop if it were spherical
+    print( rotation_speed*(density*size**3/8/surf_tens)**.5, z_tip/size, file=brown_txt)
+    print( r, rotation_speed*(density*size**3/8/surf_tens)**.5, z_tip/size)
+  plot_drop_profile('data/',f'spinNodes{lobes:01}')
